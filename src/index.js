90198d49aee9f307a0bbba6fa00cd50a793c9f70
Veeramani-Elumalai <veeramanielumalai2004@gmail.com>
2025-11-30 19:38:57 +0530

    Test - VisualCrossing API

--- src/index.js
+++ src/index.js
@@ -1,2 +1,22 @@
-console.log('Hello Form Webpack');
-console.log('this is a simple log statement');
+async function fetchData(city) {
+  try {
+    const response = await fetch(
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&contentType=json&key=SQPJWHBH3S69KVJWY4823WBHW`
+    );
+    const data = await response.json();
+    console.log(data);
+  } catch (error) {
+    console.error('Error fetching data:', error);
+  }
+}
+
+document.addEventListener('DOMContentLoaded', () => {
+  const form = document.querySelector('#searchForm');
+  const input = document.querySelector('#searchCity');
+
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const city = input.value || '';
+    fetchData(city);
+  });
+});
