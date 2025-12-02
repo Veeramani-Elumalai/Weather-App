7adda2044bcf07f082aa3f5abee632185dccb12b
Veeramani-Elumalai <veeramanielumalai2004@gmail.com>
2025-12-02 11:29:18 +0530

    Class Created for Weather report generation

--- src/index.js
+++ src/index.js
@@ -1,10 +1,50 @@
+const dataSet = [];
+
+class weather {
+  constructor(
+    celsius,
+    wind,
+    humidity,
+    percipitation,
+    description,
+    dateTime,
+    address
+  ) {
+    this.celsius = celsius;
+    this.wind = wind;
+    this.humidity = humidity;
+    this.percipitation = percipitation;
+    this.description = description;
+    this.dateTime = dateTime;
+    this.address = address;
+  }
+}
+
 async function fetchData(city) {
   try {
     const response = await fetch(
-      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}?unitGroup=us&contentType=json&key=SQPJWHBH3S69KVJWY4823WBHW`
+      `https://weather.visualcrossing.com/VisualCrossingWebServices/rest/services/timeline/${city}/today?unitGroup=metric&contentType=json&key=SQPJWHBH3S69KVJWY4823WBHW`
     );
     const data = await response.json();
-    console.log(data);
+
+    const celsius = data.currentConditions.temp;
+    const wind = data.currentConditions.windspeed;
+    const humidity = data.currentConditions.humidity;
+    const percipitation = data.currentConditions.precip;
+    const description = data.description;
+    const dateTime = data.currentConditions.datetime;
+    const address = data.resolvedAddress;
+
+    const report = new weather(
+      celsius,
+      wind,
+      humidity,
+      percipitation,
+      description,
+      dateTime,
+      address
+    );
+    dataSet.push(report);
   } catch (error) {
     console.error('Error fetching data:', error);
   }
@@ -20,3 +60,5 @@ document.addEventListener('DOMContentLoaded', () => {
     fetchData(city);
   });
 });
+
+console.log(dataSet);
